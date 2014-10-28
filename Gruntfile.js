a317ffe8375baf518b28aec30fdf150f54e67345
anami <anamicity@gmail.com>
2014-10-28 11:07:07 +0000

    bookmarklet working in Firefox - negate_iife option added to the uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,11 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
+			options: {
+				compress: {
+					negate_iife: false
+				}
+			},
 		    normal: {
 			    src: '<%= pkg.name %>.js',
 			    dest: '<%= pkg.name %>.min.js'
